f8d9cc984b5cab3583a14cca0e85a8a2a8cef4f1
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-04 09:40:45 +0700

    Get Menu Item and API Response

---
+++ API_RedMango/Controllers/MenuItemController.cs
@@ -0,0 +1,29 @@
+﻿using API_RedMango.Data;
+using API_RedMango.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API_RedMango.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result = _db.MenuItems;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
