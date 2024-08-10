2139fd5b6a204bba013aeabd9f1bce90812e5bb8
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-10 09:32:15 +0700

    Install Bootstrap and Bootstrap Icons

--- client-redmango/src/main.tsx
+++ client-redmango/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './Container/App'
 import './index.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
