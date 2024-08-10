c9b28043489d43e289eb3e97b9293c8368166ece
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-10 14:17:11 +0700

    Add Routing and Not Found

--- client-redmango/src/Container/App.tsx
+++ client-redmango/src/Container/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Footer, Header } from "../Components/Layout";
-import { Home } from "../Pages";
+import { Home, NotFound } from "../Pages";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
 
@@ -8,6 +9,12 @@ export default function App() {
     <div>
       <Header />
       <Home/>
+      <div className="pb-5">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
--- client-redmango/src/main.tsx
+++ client-redmango/src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './Container/App'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./Container/App";
+import "./index.css";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 // import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
