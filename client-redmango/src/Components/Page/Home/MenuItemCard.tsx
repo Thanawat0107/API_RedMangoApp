fdefbfcc1ac046f24e8867a1a2a2be7eecbe0d3f
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-10 10:54:54 +0700

    Home Page UI

--- client-redmango/src/Components/Page/Home/MenuItemCard.tsx
+++ client-redmango/src/Components/Page/Home/MenuItemCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { menuItemModel } from "../../../models";
-
+import { baseUrl } from "../../../Common/SD";
 type Props = {
   menuItem: menuItemModel;
 };
@@ -8,12 +8,75 @@ type Props = {
 export default function MenuItemCard({ menuItem }: Props) {
   return (
     <>
-      <div>{menuItem.id}</div>
-      <div>{menuItem.name}</div>
-      <div>{menuItem.price}</div>
-      <div>{menuItem.description}</div>
-      <div>{menuItem.category}</div>
-      <img>{menuItem.image}</img>
+        <div className="col-md-4 col-12 p-4">
+          <div
+            className="card"
+            style={{ boxShadow: "0 1px 7px 0 rgb(0 0 0 / 50%)" }}
+          >
+                <div className="card-body pt-2">
+              <div className="row col-10 offset-1 p-4">
+                <img
+                  src={baseUrl+menuItem.image}
+                  style={{ borderRadius: "50%" }}
+                  alt=""
+                  className="w-100 mt-5 image-box"
+                />
+              </div>
+              {menuItem.specialTag &&
+                menuItem.specialTag.length > 0 && (
+                  <i
+                    className="bi bi-star btn btn-success"
+                    style={{
+                      position: "absolute",
+                      top: "15px",
+                      left: "15px",
+                      padding: "5px 10px",
+                      borderRadius: "3px",
+                      outline: "none !important",
+                      cursor: "pointer",
+                    }}
+                  >
+                    &nbsp; {menuItem.specialTag}
+                  </i>
+                )}
+
+              <i
+                className="bi bi-cart-plus btn btn-outline-danger"
+                style={{
+                  position: "absolute",
+                  top: "15px",
+                  right: "15px",
+                  padding: "5px 10px",
+                  borderRadius: "3px",
+                  outline: "none !important",
+                  cursor: "pointer",
+                }}
+              ></i>
+
+              <div className="text-center">
+                <p className="card-title m-0 text-success fs-3">
+                  {menuItem.name}
+                </p>
+                <p className="badge bg-secondary" style={{ fontSize: "12px" }}>
+                  {menuItem.category}
+                </p>
+              </div>
+              <p
+                className="card-text"
+                style={{
+                  textAlign: "center",
+                  fontWeight: "light",
+                  fontSize: "14px",
+                }}
+              >
+                {menuItem.description}
+              </p>
+              <div className="row text-center">
+                <h4>${menuItem.price}</h4>
+              </div>
+            </div>
+          </div>
+        </div>
     </>
   );
 }
