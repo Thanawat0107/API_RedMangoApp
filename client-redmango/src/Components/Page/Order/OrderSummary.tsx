751d34a63ee551194d9d0d22593e15f873a1f008
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-11 16:04:48 +0700

    Dynamic Order Summary

--- client-redmango/src/Components/Page/Order/OrderSummary.tsx
+++ client-redmango/src/Components/Page/Order/OrderSummary.tsx
@@ -1,30 +1,44 @@
+import { cartItemModel } from "../../../models";
 import { orderSummaryProps } from "./orderSummaryProps";
 
 export default function OrderSummary({ data, userInput }: orderSummaryProps) {
-    return (
-      <div>
-        <h3 className="text-success">Order Summary</h3>
-        <div className="mt-3">
-          <div className="border py-3 px-2">Name : </div>
-          <div className="border py-3 px-2">Email : </div>
-          <div className="border py-3 px-2">Phone : </div>
-          <div className="border py-3 px-2">
-            <h4 className="text-success">Menu Items</h4>
-            <div className="p-3">
-              <div className="d-flex">
-                <div className="d-flex w-100 justify-content-between">
-                  <p>Menu Item Name</p>
-                  <p>$10 x 10 =</p>
+  return (
+    <div>
+      <h3 className="text-success">Order Summary</h3>
+      <div className="mt-3">
+        <div className="border py-3 px-2">Name : {userInput.name} </div>
+        <div className="border py-3 px-2">Email : {userInput.email} </div>
+        <div className="border py-3 px-2">Phone : {userInput.phoneNumber} </div>
+        <div className="border py-3 px-2">
+          <h4 className="text-success">Menu Items</h4>
+          <div className="p-3">
+           
+          {data.cartItems.map((cartItem: cartItemModel, index: number) => {
+              return (
+                <div className="d-flex">
+                  <div className="d-flex w-100 justify-content-between">
+                    <p>{cartItem.menuItem?.name}</p>
+                    <p>
+                      ${cartItem.menuItem?.price} x {cartItem.quantity} =
+                    </p>
+                  </div>
+                  <p style={{ width: "70px", textAlign: "right" }}>
+                    $
+                    {(
+                      (cartItem.menuItem?.price ?? 0) * (cartItem.quantity ?? 0)
+                    ).toFixed(2)}
+                  </p>
                 </div>
-                <p style={{ width: "70px", textAlign: "right" }}>$100</p>
-              </div>
-              <hr />
-              <h4 className="text-danger" style={{ textAlign: "right" }}>
-                $100
-              </h4>
-            </div>
+              );
+            })}
+
+            <hr />
+            <h4 className="text-danger" style={{ textAlign: "right" }}>
+            ${data.cartTotal.toFixed(2)}
+            </h4>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
