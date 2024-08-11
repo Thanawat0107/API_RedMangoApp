0ab4bcf803b4b0bf305a36a21328f9999e497618
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-11 15:04:53 +0700

    Use Location State

--- client-redmango/src/Pages/Payment.tsx
+++ client-redmango/src/Pages/Payment.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 export default function Payment() {
-  return (
-    <div>Payment</div>
-  )
+  const {
+    state: { apiResult, userInput },
+  } = useLocation();
+
+  console.log(apiResult);
+  console.log(userInput);
+
+  return <div>Payment</div>;
 }
