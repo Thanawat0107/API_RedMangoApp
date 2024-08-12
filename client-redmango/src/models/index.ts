38d387fda1ed31edf81610b9b618e55a9d434d67
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-12 11:06:22 +0700

    Order Interface

--- client-redmango/src/models/index.ts
+++ client-redmango/src/models/index.ts
@@ -3,5 +3,15 @@ import { cartItemModel } from "./cartItemModel";
 import { shoppingCartModel } from "./shoppingCartModel";
 import { userModel } from "./userModel";
 import { apiResponse } from "./apiResponse";
+import { orderDetailModel } from "./orderDetailModel";
+import { orderHeaderModel } from "./orderHeaderModel";
 
-export type { menuItemModel, cartItemModel, shoppingCartModel, userModel, apiResponse };
+export type {
+  menuItemModel,
+  cartItemModel,
+  shoppingCartModel,
+  userModel,
+  apiResponse,
+  orderDetailModel,
+  orderHeaderModel,
+};
