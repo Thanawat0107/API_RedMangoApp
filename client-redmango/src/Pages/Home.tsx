06e1d19609c263b53e266b17b0e10354d628b0d9
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-13 08:21:36 +0700

    Section 09 Client – Home UI  Banner

--- client-redmango/src/Pages/Home.tsx
+++ client-redmango/src/Pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { MenuItemList } from "../Components/Page/Home";
+import { Banner } from "../Common";
 
 export default function Home() {
   return (
     <>
+      <Banner />
       <div className="container p-2">
         <MenuItemList />
       </div>
