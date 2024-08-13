ae204c40d5f90e38143d64f1fb80a385221acb12
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-13 10:33:57 +0700

    Input Controller Components

--- client-redmango/src/Pages/Order/AllOrders.tsx
+++ client-redmango/src/Pages/Order/AllOrders.tsx
@@ -2,31 +2,59 @@ import { withAdminAuth } from "../../HOC";
 import { useGetAllOrdersQuery } from "../../Apis/orderApi";
 import OrderList from "../../Components/Page/Order/OrderList";
 import { MainLoader } from "../../Common";
+import { SD_Status } from "../../Common/SD";
+import { useState } from "react";
+import { inputHelper } from "../../Helper";
+
+const filterOptions = [
+  "All",
+  SD_Status.CONFIRMED,
+  SD_Status.BEING_COOKED,
+  SD_Status.READY_FOR_PICKUP,
+  SD_Status.CANCELLED,
+];
 
 function AllOrders() {
   const { data, isLoading } = useGetAllOrdersQuery("");
+  const [filters, setFilters] = useState({ searchString: "", status: "" });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const tempValue = inputHelper(e, filters);
+    setFilters(tempValue);
+  };
+
   return (
     <>
       {isLoading && <MainLoader />}
       {!isLoading && (
          <>
-         <div className="d-flex align-items-center justify-content-between mx-5 mt-5">
-           <h1 className="text-success">Orders List</h1>
-           <div className="d-flex" style={{ width: "40%" }}>
-             <input
-               type="text"
-               className="form-control mx-2"
-               placeholder="Search Name, Email or Phone"
-             />
-             <select className="form-select w-50 mx-2">
-               <option value="All">All</option>
-             </select>
-             <button className="btn btn-outline-success">Filter</button>
-           </div>
-         </div>
+          <div className="d-flex align-items-center justify-content-between mx-5 mt-5">
+            <h1 className="text-success">Orders List</h1>
+            <div className="d-flex" style={{ width: "40%" }}>
+              <input
+                type="text"
+                className="form-control mx-2"
+                placeholder="Search Name, Email or Phone"
+                name="searchString"
+                onChange={handleChange}
+              />
+                <select
+                className="form-select w-50 mx-2"
+                onChange={handleChange}
+                name="status"
+              >
+                {filterOptions.map((item) => (
+                  <option value={item === "All" ? "" : item}>{item}</option>
+                ))}
+              </select>
+              <button className="btn btn-outline-success">Filter</button>
+            </div>
+          </div>
 
-         <OrderList isLoading={isLoading} orderData={data.result} />
-       </>
+          <OrderList isLoading={isLoading} orderData={data.result} />
+        </>
       )}
     </>
   );
