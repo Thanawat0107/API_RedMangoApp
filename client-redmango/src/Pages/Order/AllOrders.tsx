a5cfbbbe5c6046fa9a67cea2b5b6e670fbca727d
Thanawat0107 <qwwasszxx@gmail.com>
2024-08-13 10:39:53 +0700

    Local Filters in Action

--- client-redmango/src/Pages/Order/AllOrders.tsx
+++ client-redmango/src/Pages/Order/AllOrders.tsx
@@ -3,8 +3,9 @@ import { useGetAllOrdersQuery } from "../../Apis/orderApi";
 import OrderList from "../../Components/Page/Order/OrderList";
 import { MainLoader } from "../../Common";
 import { SD_Status } from "../../Common/SD";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { inputHelper } from "../../Helper";
+import { orderHeaderModel } from "../../models";
 
 const filterOptions = [
   "All",
@@ -15,6 +16,7 @@ const filterOptions = [
 ];
 
 function AllOrders() {
+  const [orderData, setOrderData] = useState([]);
   const { data, isLoading } = useGetAllOrdersQuery("");
   const [filters, setFilters] = useState({ searchString: "", status: "" });
 
@@ -25,11 +27,38 @@ function AllOrders() {
     setFilters(tempValue);
   };
 
+  const handleFilters = () => {
+    const tempData = data.result.filter((orderData: orderHeaderModel) => {
+      if (
+        (orderData.pickupName &&
+          orderData.pickupName.includes(filters.searchString)) ||
+        (orderData.pickupEmail &&
+          orderData.pickupEmail.includes(filters.searchString)) ||
+        (orderData.pickupPhoneNumber &&
+          orderData.pickupPhoneNumber.includes(filters.searchString))
+      ) {
+        return orderData;
+      }
+    });
+
+    const finalArray = tempData.filter((orderData: orderHeaderModel) =>
+      filters.status !== "" ? orderData.status === filters.status : orderData
+    );
+
+    setOrderData(finalArray);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setOrderData(data.result);
+    }
+  }, [data]);
+
   return (
     <>
       {isLoading && <MainLoader />}
       {!isLoading && (
-         <>
+        <>
           <div className="d-flex align-items-center justify-content-between mx-5 mt-5">
             <h1 className="text-success">Orders List</h1>
             <div className="d-flex" style={{ width: "40%" }}>
@@ -40,20 +69,27 @@ function AllOrders() {
                 name="searchString"
                 onChange={handleChange}
               />
-                <select
+              <select
                 className="form-select w-50 mx-2"
                 onChange={handleChange}
                 name="status"
               >
-                {filterOptions.map((item) => (
-                  <option value={item === "All" ? "" : item}>{item}</option>
+                {filterOptions.map((item, index) => (
+                  <option key={index} value={item === "All" ? "" : item}>
+                    {item}
+                  </option>
                 ))}
               </select>
-              <button className="btn btn-outline-success">Filter</button>
+              <button
+                onClick={handleFilters}
+                className="btn btn-outline-success"
+              >
+                Filter
+              </button>
             </div>
           </div>
 
-          <OrderList isLoading={isLoading} orderData={data.result} />
+          <OrderList isLoading={isLoading} orderData={orderData} />
         </>
       )}
     </>
